e1ec2b828ccf42ab3791661c3b2b51df6ad27602
Marc Villacorta <marc.villacorta@gmail.com>
2018-09-11 00:17:29 +0200

    Set sensor resolution to 12

--- waterwell.ino
+++ waterwell.ino
@@ -2,21 +2,23 @@
 #include <DallasTemperature.h>
 
 #define ONE_WIRE_BUS 2
+#define SENSOR_RESOLUTION 12
+#define SENSOR_INDEX 0
 
 OneWire oneWire(ONE_WIRE_BUS);
 DallasTemperature sensors(&oneWire);
+DeviceAddress sensorDeviceAddress;
 
 void setup(void) {
   Serial.begin(9600);
-  Serial.println("Dallas Temperature IC Control Library Demo");
   sensors.begin();
+  sensors.getAddress(sensorDeviceAddress, SENSOR_INDEX);
+  sensors.setResolution(sensorDeviceAddress, SENSOR_RESOLUTION);
 }
 
 void loop(void) {
-  Serial.print(" Requesting temperatures...");
   sensors.requestTemperatures();
-  Serial.println("DONE");
   Serial.print("Temperature is: ");
-  Serial.print(sensors.getTempCByIndex(0));
-  delay(1000);
+  Serial.println(sensors.getTempCByIndex(SENSOR_INDEX), 4);
+  delay(3000);
 }
