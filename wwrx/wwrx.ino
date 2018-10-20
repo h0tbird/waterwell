cc3ead23176311f754e8265fb28114d1c64b0584
Marc Villacorta <marc.villacorta@gmail.com>
2018-10-20 19:40:43 +0200

    Added new 'printSensors' function

--- wwrx/wwrx.ino
+++ wwrx/wwrx.ino
@@ -13,7 +13,7 @@ void loop(void) {
   uint8_t buflen = sizeof(buf);
 
   if (rf_driver.recv(buf, &buflen)) {
-    Serial.print("Temperature is: ");
+    Serial.print("Temperature RX: ");
     Serial.println((char *)buf);
   }
 }
--- wwtx/wwtx.ino
+++ wwtx/wwtx.ino
@@ -14,16 +14,67 @@ String str_temp;
 // Initialize:
 OneWire oneWire(ONE_WIRE_BUS);
 DallasTemperature sensors(&oneWire);
-DeviceAddress sensorDeviceAddress;
+DeviceAddress deviceAddress;
 RH_ASK rf_driver;
 
-void setup(void) {
-  Serial.begin(9600);
-  sensors.begin();
-  sensors.getAddress(sensorDeviceAddress, SENSOR_INDEX);
-  sensors.setResolution(sensorDeviceAddress, SENSOR_RESOLUTION);
-  rf_driver.init();
+// Sensors:
+DeviceAddress sensor00 = { 0x28, 0xB, 0xCA, 0x77, 0x91, 0x9, 0x2, 0xDA };
+
+//-----------------------------------------------------------------------------
+// printSensors
+//-----------------------------------------------------------------------------
+
+void printSensors(void){
+
+  uint8_t deviceCount;
+  byte dev,addr[8],i;
+
+  // Count sensors:
+  deviceCount = sensors.getDeviceCount();
+  Serial.print("Device count: ");
+  Serial.println(deviceCount);
+
+  for(dev = 0; dev < deviceCount; dev++) {
+
+    Serial.print("device-");
+    Serial.print(dev);
+    Serial.print(" =");
+
+    sensors.getAddress(addr, (uint8_t)dev);
+
+    for (i = 0; i < 8; i++) {
+      Serial.write(' ');
+      Serial.print(addr[i], HEX);
+    }
+
+    Serial.println();
+  }
 }
+
+//-----------------------------------------------------------------------------
+// setup
+//-----------------------------------------------------------------------------
+
+void setup(void) {
+
+  // Serial:
+  Serial.begin(9600);
+
+  // DS18B20:
+  sensors.begin();
+  sensors.getAddress(deviceAddress, SENSOR_INDEX);
+  sensors.setResolution(deviceAddress, SENSOR_RESOLUTION);
+
+  // 433Mhz:
+  rf_driver.init();
+
+  // Info:
+  printSensors();
+}
+
+//-----------------------------------------------------------------------------
+// loop
+//-----------------------------------------------------------------------------
 
 void loop(void) {
 
@@ -33,7 +84,7 @@ void loop(void) {
   str_temp = String(temp);
 
   // Print to serial console:
-  Serial.print("Temperature is: ");
+  Serial.print("Temperature TX: ");
   Serial.println(str_temp);
 
   // Transfer via 433MHz:
